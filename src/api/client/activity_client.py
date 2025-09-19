778421115a2c78d01c174d4b93ad3bfdcf550645
Denis Malinin <melowetty@mail.ru>
2025-09-19 16:02:28 +0500

    fix copy visits

--- src/api/client/activity_client.py
+++ src/api/client/activity_client.py
@@ -22,7 +22,7 @@ class ActivityClient(BaseClient):
             return await client._get_request_or_error(url)
 
     async def copy_activity_visits(self, from_activity_id: int, to_activity_id: int) -> None | Error:
-        url = f"/activities/{from_activity_id}/visits-copy/${to_activity_id}"
+        url = f"/activities/{from_activity_id}/visits-copy/{to_activity_id}"
         client: ActivityClient
         async with self as client:
             return await client._post_request_or_error(url)
